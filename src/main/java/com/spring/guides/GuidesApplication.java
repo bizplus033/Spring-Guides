79f82855a1d39184a2ee4cbb7c706a243c7e6aea
bizplus033 <bizplus033@naver.com>
2024-02-14 17:05:36 +0900

    6. JdbcTemplate

--- src/main/java/com/spring/guides/GuidesApplication.java
+++ src/main/java/com/spring/guides/GuidesApplication.java
@@ -1,40 +1,60 @@
 package com.spring.guides;
 
 import com.spring.guides.ch3_consumingrest.Quote;
+import com.spring.guides.ch6_jdbctemplate.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @EnableScheduling
 @SpringBootApplication
-public class GuidesApplication {
+public class GuidesApplication implements CommandLineRunner{
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 	private static final Logger log = LoggerFactory.getLogger(GuidesApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuidesApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 
-	@Bean
-	@Profile("!test")
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"http://localhost:8080/api/random", Quote.class);
-			log.info(quote.toString());
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Creating tables");
 
-		};
+		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+		jdbcTemplate.execute("CREATE TABLE customers(" +
+				"id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+		List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+				.map(name -> name.split(" "))
+				.collect(Collectors.toList());
+
+		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?, ?)", splitUpNames);
+
+		log.info("Querying for customer records where first_name = 'Josh':");
+		List<Customer> content = jdbcTemplate.query(
+				"SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+				(rs, rowNum)
+						-> new Customer(rs.getLong("id"),
+						rs.getString("first_name"),
+						rs.getString("last_name")), "Josh");
+		content.forEach(c -> log.info(c.toString()));
 	}
 }
